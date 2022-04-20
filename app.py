fbddf41169c8c198161b98ebe250f7fd882b7c8c
VampSlayer <sayam+git@momentumdash.com>
2022-04-20 22:48:21 +0100

    Add more Forbes 100 quotes

--- app.py
+++ app.py
@@ -97,7 +97,35 @@ quotes = {
         ],
     1: [{'quote': 'There is no fear for one whose mind is not filled with desires.', 'source': 'Buddha'}],
     2: [{'quote': 'Raise your words, not voice. It is rain that grows flowers, not thunder.', 'source': 'Rumi'}],
-    3: [{'quote': 'Life is about making an impact, not making an income.', 'source': 'Kevin Kruse'}],
+    3: [{'quote': 'Life is about making an impact, not making an income.', 'source': 'Kevin Kruse'},
+        {'quote': 'Whatever the mind of man can conceive and believe, it can achieve.', 'source': 'Napoleon Hill'},
+        {'quote': 'Strive not to be a success, but rather to be of value.', 'source': 'Albert Einstein'},
+        {
+            'quote': 'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.',
+            'source': 'Robert Frost'},
+        {'quote': 'I attribute my success to this: I never gave or took any excuse.', 'source': 'Florence Nightingale'},
+        {'quote': 'You miss 100% of the shots you don’t take.', 'source': 'Wayne Gretzky'},
+        {
+            'quote': "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
+            'source': 'Michael Jordan'},
+        {'quote': 'The most difficult thing is the decision to act, the rest is merely tenacity.',
+         'source': 'Amelia Earhart'},
+        {'quote': 'Definiteness of purpose is the starting point of all achievement.', 'source': 'W. Clement Stone'},
+        {'quote': "Life isn't about getting and having, it's about giving and being.", 'source': 'Kevin Kruse'},
+        {'quote': "Life is what happens to you while you’re busy making other plans.", 'source': 'John Lennon'},
+        {'quote': "We become what we think about.", 'source': 'Earl Nightingale'},
+        {
+            'quote': "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.",
+            'source': 'Mark Twain'},
+        {'quote': "Life is 10% what happens to me and 90% of how I react to it.", 'source': 'Charles Swindoll'},
+        {'quote': "The most common way people give up their power is by thinking they don’t have any.",
+         'source': 'Alice Walker'},
+        {'quote': "The mind is everything. What you think you become.", 'source': 'Buddha'},
+        {'quote': "The best time to plant a tree was 20 years ago. The second best time is now.",
+         'source': 'Chinese Proverb'},
+        {'quote': "An unexamined life is not worth living.", 'source': 'Socrates'},
+        {'quote': "Eighty percent of success is showing up.", 'source': 'Woody Allen'},
+        ],
 }
 
 quote_parser = reqparse.RequestParser()
